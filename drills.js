a37ab2627ff2c3d2978f579ca41e6ba7745dfca6
kathyleboeuf <kleboeuf@townisp.com>
2018-10-11 12:33:54 -0400

    added mergeSort

--- drills.js
+++ drills.js
@@ -131,4 +131,41 @@ const partition = (array, start, end) => {
   return j;
 };
 
-console.log(qSort(numArr));
+// console.log(qSort(numArr));
+//==========================
+
+const mSort = (array) => {
+  if (array.length <= 1) {
+    return array;
+  }
+  const middle = Math.floor(array.length / 2);
+  let left = array.slice(0, middle);
+  let right = array.slice(middle, array.length);
+
+  left = mSort(left);
+  right = mSort(right);
+  return merge(left, right, array);
+}
+const merge = (left, right, array) => {
+  let leftIndex = 0;
+  let rightIndex = 0;
+  let outputIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      array[outputIndex++] = left[leftIndex++];
+    }
+    else {
+      array[outputIndex++] = right[rightIndex++];
+    }
+  }
+  for (let i = leftIndex; i < left.length; i++) {
+    array[outputIndex++] = left[i];
+  }
+  for (let i = rightIndex; i < right.length; i++) {
+    array[outputIndex++] = right[i];
+  }
+  return array;
+};
+
+console.log(mSort(numArr));
